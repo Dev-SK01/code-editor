10999934d14ab27eba3c73438b51c7733108180c
devsk <devsk@devsk>
2024-11-30 17:06:43 +0530

    eslint disables and codemirror disabled

--- code-editor/src/app/CodeEditor.tsx
+++ code-editor/src/app/CodeEditor.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import CodeMirror from 'codemirror';
+// import CodeMirror from 'codemirror';
 import 'codemirror/mode/python/python';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/clike/clike';
@@ -17,28 +17,29 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ language, value, onChange, className }) => {
-  const editorRef = useRef<CodeMirror.Editor | null>();
+  const editorRef = useRef();
+  // <CodeMirror.Editor | null>
 
   useEffect(() => {
     if (editorRef.current === null) {
-      const editor = CodeMirror(editorRef.current, {
-        mode: language,
-        theme: 'material',
-        lineNumbers: true,
-        tabSize: 2,
-        indentUnit: 2,
-        smartIndent: true,
-        value,
-      });
+      // const editor = CodeMirror(editorRef.current, {
+      //   mode: language,
+      //   theme: 'material',
+      //   lineNumbers: true,
+      //   tabSize: 2,
+      //   indentUnit: 2,
+      //   smartIndent: true,
+      //   value,
+      // });
 
-      editor.current.on('change', (editor) => {
-        onChange(editor.getValue());
-      });
-      editor.current.toTextArea();
+      // editor.current.on('change', (editor:any) => {
+      //   onChange(editor.getValue());
+      // });
+      // editor.current.toTextArea();
     };
   }, [language, value, onChange]);
 
-  return <textarea name="code" id="text" className={className} ref={editorRef}>{value}</textarea>;
+  return <textarea name="code" id="text" className={className} >{value}</textarea>;
 };
 
 export default CodeEditor;
